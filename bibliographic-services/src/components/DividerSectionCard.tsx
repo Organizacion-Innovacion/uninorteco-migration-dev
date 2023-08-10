e158a65b8e49af402074bead9b2473dbd2eafe23
dgop92 <inevaup@gmail.com>
2023-08-10 14:21:00 -0500

    feat: add bibliographic-services card

---
+++ bibliographic-services/src/components/DividerSectionCard.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Typography, Divider } from "@ellucian/react-design-system/core";
+// TODO: if posible do not use @mui/material
+import Stack from "@mui/material/Stack";
+import { SxProps } from "@mui/system";
+
+export interface DividerSectionCardProps {
+  title: string;
+  stackProps?: SxProps;
+}
+
+export function DividerSectionCard({ title, stackProps }: DividerSectionCardProps) {
+  return (
+    <Stack direction="row" sx={{ gap: 1, alignItems: "center", ...stackProps }}>
+      <Divider sx={{ width: "5%" }} />
+      <Typography variant="body1" color="textSecondary">
+        {title}
+      </Typography>
+      <Divider sx={{ flexGrow: 1 }} />
+    </Stack>
+  );
+}

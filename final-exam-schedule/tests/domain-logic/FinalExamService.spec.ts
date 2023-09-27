1716222b0c73fd014561ee2e90198ee77a213bf7
Luis Fernando Galviz <lgalviz@uninorte.edu.co>
2023-09-27 08:02:14 -0500

    add test for finalexamservice

--- final-exam-schedule/tests/domain-logic/FinalExamService.spec.ts
+++ final-exam-schedule/tests/domain-logic/FinalExamService.spec.ts
@@ -1,64 +1,118 @@
 import { FinalExamService } from "../../src/core/domain-logic/final-exam-domain";
-import { DateTimeService } from "../../src/core/domain-logic/final-exam-domain";
-import { ExamSortingService } from "../../src/core/domain-logic/final-exam-domain";
+import { MyRepository } from "../../src/core/repositories/repo-rest";
+import { DateTimeService } from "../../src/core/domain-logic/date-services";
+import { ExamSortingService } from "../../src/core/domain-logic/sort-services";
+import { sampleExams, expectedSampleExams } from "./test-data";
 
-// Mock del repository (puedes implementar uno si es necesario)
-class MockRepository {
-  async getAllFinalExams() {
-    // Implementa una función mock para getAllFinalExams si es necesario
-  }
-}
+// Mock de MyRepository
+jest.mock("../../src/core/repositories/repo-rest");
+const MockedMyRepository = MyRepository as jest.MockedClass<typeof MyRepository>;
+
+// Mock de DateTimeService
+jest.mock("../../src/core/domain-logic/date-services");
+const MockedDateTimeService = DateTimeService as jest.Mocked<typeof DateTimeService>;
+
+// Mock de ExamSortingService
+jest.mock("../../src/core/domain-logic/sort-services");
+const MockedExamSortingService = ExamSortingService as jest.Mocked<
+  typeof ExamSortingService
+>;
 
 describe("FinalExamService", () => {
   let finalExamService: FinalExamService;
 
   beforeEach(() => {
-    finalExamService = new FinalExamService(new MockRepository()); // Puedes proporcionar un mock del repository
+    finalExamService = new FinalExamService();
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
   describe("getGroupExamByDate", () => {
+    /* it("should return null if finalExamResponse is null", async () => {
+      MockedMyRepository.prototype.getAllFinalExams.mockResolvedValue(null);
+
+      const result = await finalExamService.getGroupExamByDate();
+
+      expect(result).toBeNull();
+    }); */
+
+    it("should return null if allExams is empty", async () => {
+      MockedMyRepository.prototype.getAllFinalExams.mockResolvedValue({
+        codigo: "200",
+        descripcion: "Éxito",
+        resultado: [],
+      });
+
+      const result = await finalExamService.getGroupExamByDate();
+
+      expect(result).toBeNull();
+    });
+
     it("should return grouped and sorted exams", async () => {
-      // Implementa pruebas para getGroupExamByDate aquí
+      const getAllFinalExamsResult = {
+        codigo: "200",
+        descripcion: "Éxito",
+        resultado: sampleExams,
+      };
+
+      MockedMyRepository.prototype.getAllFinalExams.mockResolvedValue(
+        getAllFinalExamsResult
+      );
+
+      // Mock para ExamSortingService.sortExamsByDate
+      const sortedExamsMock = sampleExams;
+      MockedExamSortingService.sortExamsByDate.mockReturnValue(sortedExamsMock);
+
+      // Mock para ExamSortingService.sortExamsByHour
+      const examsByHourMock = {
+        // ... Exámenes agrupados por hora ...
+      };
+      MockedExamSortingService.sortExamsByHour.mockReturnValue(examsByHourMock);
+
+      const result = await finalExamService.getGroupExamByDate();
+
+      expect(result).toEqual(examsByHourMock);
     });
   });
 
   describe("getNextExam", () => {
-    it("should return the next upcoming exam", async () => {
-      // Implementa pruebas para getNextExam aquí
-    });
-  });
-});
+    it("should return null if getGroupExamByDate returns null", async () => {
+      const getGroupExamByDateMock = jest.spyOn(finalExamService, "getGroupExamByDate");
+      getGroupExamByDateMock.mockResolvedValue(null);
 
-describe("DateTimeService", () => {
-  describe("getCurrentDate", () => {
-    it("should return the current date in 'dd/mm/yyyy' format", () => {
-      // Implementa pruebas para getCurrentDate aquí
-    });
-  });
+      const result = await finalExamService.getNextExam();
 
-  describe("dateCompare", () => {
-    it("should compare two dates correctly", () => {
-      // Implementa pruebas para dateCompare aquí
+      expect(result).toBeNull();
     });
-  });
+    /*
+    it("should return the closest upcoming exam", async () => {
+      const currentDateMock = "25/05/2023";
+      const currentTimeMock = "10:00";
 
-  describe("compareTimes", () => {
-    it("should compare two times correctly", () => {
-      // Implementa pruebas para compareTimes aquí
-    });
-  });
-});
+      // Mock para DateTimeService.getCurrentDate
+      MockedDateTimeService.getCurrentDate.mockReturnValue(currentDateMock);
 
-describe("ExamSortingService", () => {
-  describe("sortExamsByDate", () => {
-    it("should sort exams by date", () => {
-      // Implementa pruebas para sortExamsByDate aquí
-    });
-  });
+      // Mock para DateTimeService.getCurrentTime
+      MockedDateTimeService.getCurrentTime.mockReturnValue(currentTimeMock);
 
-  describe("sortExamsByHour", () => {
-    it("should sort exams by hour", () => {
-      // Implementa pruebas para sortExamsByHour aquí
-    });
+      const groupedExamsMock = {
+        "25/05/2023": [
+          // ... Exámenes para la fecha actual ...
+        ],
+        "26/05/2023": [
+          // ... Exámenes para futuras fechas ...
+        ],
+      };
+
+      const getGroupExamByDateMock = jest.spyOn(finalExamService, "getGroupExamByDate");
+      getGroupExamByDateMock.mockResolvedValue(groupedExamsMock);
+
+      const result = await finalExamService.getNextExam();
+
+      // Asegúrate de que result sea el examen correcto según tus datos de prueba
+      expect(result).toEqual(expectedSampleExams);
+    }); */
   });
 });
